287d78ae87a1d4eb018458293845eff5697638ea
Denis Timkov <denistimkov@gmail.com>
2022-12-04 23:29:26 -0300

    supervisor

--- model_dl.py
+++ model_dl.py
@@ -4,7 +4,8 @@ import gdown
 
 parser = argparse.ArgumentParser(description='.')
 parser.add_argument('-u', '--url')
+parser.add_argument('-n', '--modelname', default="model.ckpt")
 
 args = parser.parse_args()
 if args.url:
-    gdown.download(args.url, quiet=False, output="models/Stable-diffusion/model.ckpt")
+    gdown.download(args.url, quiet=False, output=f"models/Stable-diffusion/{args.modelname}")
